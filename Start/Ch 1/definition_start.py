e41b77e26c5060825076569923f2347ec0c0336a
chadhumble <chadhumble@gmail.com>
2024-01-14 15:23:12 -0700

    Added to Ch1_definition_start

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,16 @@
 
 
 # TODO: create a basic class
-
+# The __init__ is also known as the initializer function in Python. 
+# It is the first thing that is called in a class before other functions.
+class Book:
+  def __init__(self, title):
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("Brave New World")
+book2 = Book("War and Peace")
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
