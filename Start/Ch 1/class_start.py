fed6c31183c04d5c555188880dd9b945663b15c2
Mir-Aadil1 <ermiraadil@gmail.com>
2024-02-08 09:45:25 +0530

    added calss_start.py with use of static method

--- Start/Ch 1/class_start.py
+++ Start/Ch 1/class_start.py
@@ -4,26 +4,43 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
-
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
     # TODO: double-underscore properties are hidden from other classes
-
+    __booklist = None
     # TODO: create a class method
+    @classmethod
+    def get_book_types(cls):
+        return cls.BOOK_TYPES
 
     # TODO: create a static method
+    @staticmethod
+    def getbooklist():
+        if Book.__booklist == None:
+            Book.__booklist = []
+        return Book.__booklist
+
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def set_title(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
-
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} is not a valid book type")
+        else:
+            self.booktype = booktype
 
 # TODO: access the class attribute
-
+print("Book types: ", Book.get_book_types())
 
 # TODO: Create some book instances
-
+b1 = Book("Title1", "HARDCOVER")
+b2 = Book("Title1", "PAPERBACK")
 
 # TODO: Use the static method to access a singleton object
+thebooks = Book.getbooklist()
+thebooks.append(b1)
+thebooks.append(b2)
+print(thebooks)
