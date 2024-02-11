e58340042c922eaadbf7e205ddc4543305051e2b
chadhumble <chadhumble@gmail.com>
2024-02-11 09:13:15 -0700

    Update from finishing segements.

--- Start/Ch 2/interface_start.py
+++ Start/Ch 2/interface_start.py
@@ -12,14 +12,22 @@ class GraphicShape(ABC):
     def calcArea(self):
         pass
 
+class JSONify(ABC):
+    @abstractmethod
+    def toJSON(self):
+        pass
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
 
+    def toJSON(self):
+        return f"{{'Circle': {str(self.calcArea())}}}"
+
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
--- Start/Ch 2/multiple_start.py
+++ Start/Ch 2/multiple_start.py
@@ -1,22 +1,31 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Understanding multiple inheritance
 
-
 class A:
     def __init__(self):
         super().__init__()
         self.prop1 = "prop1"
-
+        self.name = "Class A"
 
 class B:
     def __init__(self):
         super().__init__()
         self.prop2 = "prop2"
+        self.name = "Class B"
 
 
+# Class C is our class that will inherit from the base classes A and B. 
+# It does this by separating them with a comma within parenthesis.
 class C(A, B):
     def __init__(self):
         super().__init__()
+    
+    def showprops(self):
+        print(self.prop1)
+        print(self.prop2)
+        print(self.name)
 
 
 c = C()
+print(C.__mro__)
+c.showprops()
