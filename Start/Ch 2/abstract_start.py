9db1b5a5f4373d1535623d2992db2dad3da8434e
chadhumble <chadhumble@gmail.com>
2024-02-10 18:33:12 -0700

    Class update ran from VScode

--- Start/Ch 2/abstract_start.py
+++ Start/Ch 2/abstract_start.py
@@ -1,11 +1,20 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using Abstract Base Classes to enforce class constraints
+# We will create a base class that lets users create different kind of 2 deminsional shapes.
 
+# We want to enforce the Circle and Square classes to inherit the calcArea from class GraphicShape.
+# We also want to prevent the class GraphicShape itself from being instantiated.
 
-class GraphicShape:
+# Import the abc method from the python standard library. The ABC is abstract base methods we import
+# along with abstractmethod.
+# We inherit abstractmethod from the ABC class for GraphicShape.
+
+from abc import ABC, abstractmethod 
+class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
-
+# adding the decorator @ with abstractmethod means classes must inherit calcArea.
+    @abstractmethod 
     def calcArea(self):
         pass
 
@@ -13,14 +22,17 @@ class GraphicShape:
 class Circle(GraphicShape):
     def __init__(self, radius):
         self.radius = radius
+    
+    def calcArea(self):
+        return 3.14 * (self.radius ** 2)
 
 
 class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
+    def calcArea(self):
+        return self.side * self.side
 
-
-g = GraphicShape()
 
 c = Circle(10)
 print(c.calcArea())
