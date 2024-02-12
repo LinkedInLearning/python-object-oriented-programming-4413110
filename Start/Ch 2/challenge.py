482a7a186fc5c23368411a6a3b04693a08fcdb68
chadhumble <chadhumble@gmail.com>
2024-02-12 07:29:09 -0700

    Update after finishing segment.

--- Start/Ch 2/challenge.py
+++ Start/Ch 2/challenge.py
@@ -12,15 +12,33 @@
 # For stocks: "Ticker: Company -- $Price"
 # For bonds: "description: duration'yr' : $price : yieldamt%"
 
-class Asset():
-    pass
+from abc import ABC, abstractmethod
+class Asset(ABC):
+    def __init__(self, price):
+        self.price = price
+    
+    @abstractmethod
+    def get_description(self):
+        pass
 
-class Stock():
-    pass
+class Stock(Asset):
+    def __init__(self, ticker, price, company):
+        super().__init__(price)
+        self.company = company
+        self.ticker = ticker
 
-class Bond():
-    pass
-
+    def get_description(self):
+        return f"{self.ticker}: {self.company} -- ${self.price}"
+        
+class Bond(Asset):
+    def __init__(self, price, description, duration, yieldamt):
+        super().__init__(price)
+        self.description = description
+        self.duration = duration
+        self.yiedlamt = yieldamt
+    
+    def get_description(self):
+        return f"{self.description}: {self.duration}yr: ${self.price} : {self.yiedlamt}%"
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
 try:
--- Start/Ch 2/composition_start.py
+++ Start/Ch 2/composition_start.py
@@ -1,25 +1,42 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using composition to build complex objects
 
-
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author=None):
         self.title = title
         self.price = price
-
-        self.authorfname = authorfname
-        self.authorlname = authorlname
-
+        self.author = author
         self.chapters = []
 
-    def addchapter(self, name, pages):
-        self.chapters.append((name, pages))
+    def addchapter(self, chapter):
+        self.chapters.append(chapter)
+    
+    def getbookpagecount(self):
+        result = 0
+        for ch in self.chapters:
+            result += ch.pagecount
+        return result
 
+class Author:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
 
-b1 = Book("War and Peace", 39.0, "Leo", "Tolstoy")
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
 
-b1.addchapter("Chapter 1", 125)
-b1.addchapter("Chapter 2", 97)
-b1.addchapter("Chapter 3", 143)
+class Chapter:
+    def __init__(self, name, pagecount):
+        self.name = name
+        self.pagecount = pagecount
+
+auth = Author("Leo","Tolstoy")
+b1 = Book("War and Peace", 39.0, auth)
+
+b1.addchapter(Chapter("Chapter 1", 125))
+b1.addchapter(Chapter("Chapter 2", 97))
+b1.addchapter(Chapter("Chapter 3", 143))
 
 print(b1.title)
+print(b1.author)
+print(b1.getbookpagecount())
