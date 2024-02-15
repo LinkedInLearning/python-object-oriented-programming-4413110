8821c5d122b818b2891ee78cdf947ca8c26edc1a
SHypoleac <153895174+SHypoleac@users.noreply.github.com>
2024-02-15 09:45:26 +0100

    Update datadefault_start.py
    additional description

--- Start/Ch 4/datadefault_start.py
+++ Start/Ch 4/datadefault_start.py
@@ -1,7 +1,11 @@
 # Python Object Oriented Programming by Joe Marini course example
 # implementing default values in data classes
 
+# TODO: Import "field" function from dataclasses
 from dataclasses import dataclass
+
+# TODO: Define a function that returns a random number between 20-40.
+# Don't forget to import the necessary module
 
 
 @dataclass
@@ -11,3 +15,4 @@ class Book:
     author: str
     pages: int
     price: float
+    # TODO: Use the field function to get default price as random number
