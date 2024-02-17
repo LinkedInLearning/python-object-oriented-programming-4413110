1103d6c9531908c7bb381d417f91a113ee203aa7
chadhumble <chadhumble@gmail.com>
2024-02-17 14:51:36 -0700

    Finished sections of class.

--- Start/Ch 3/magiceq_start.py
+++ Start/Ch 3/magiceq_start.py
@@ -10,10 +10,28 @@ class Book:
         self.price = price
 
     # TODO: the __eq__ method checks for equality between two objects
+    def __eq__(self, value):
+        if not isinstance(value,Book):
+            raise ValueError("Can't compare book to a non-book")
+
+        return (self.title == value.title and 
+                self.author == value.author and self.price == value.price)
 
     # TODO: the __ge__ establishes >= relationship with another obj
+    def __ge__(self,value):
+         if not isinstance(value,Book):
+            raise ValueError("Can't compare book to a non-book")
+         
+         return (self.title == value.title and 
+                 self.author == value.author and 
+                 self.price == value.price)
+       
 
     # TODO: the __lt__ establishes < relationship with another obj
+    def __lt__(self,value):
+         if not isinstance(value,Book):
+            raise ValueError("Can't compare book to a non-book")
+         return self.price > value.price
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
@@ -22,9 +40,18 @@ b3 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b4 = Book("To Kill a Mockingbird", "Harper Lee", 24.95)
 
 # TODO: Check for equality
-
+#print(b1 == b3)
+#print(b1 == b2)
+# print(b1 == 42)
 
 # TODO: Check for greater and lesser value
-
+#print(b2 >= b1)
+#print(b2 < b1)
+    
 
 # TODO: Now we can sort them too
+books = [b1,b2,b3,b4]
+books.sort()
+print([book.title for book in books])
+
+# Refer to https://docs.python.org/3/reference/datamodel.html
--- Start/Ch 3/magicstr_start.py
+++ Start/Ch 3/magicstr_start.py
@@ -1,6 +1,12 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using the __str__ and __repr__ magic methods
 
+# The str function is used to provide a user-friendly string description of the object and is
+# usually intended to be displayed to the user.
+
+# The repr function is used to generate a more developer facing string that ideally can be
+# used to recreate the object in its current state. It's also commonly used for debugging purposes.
+# It gets used a lot to display detailed information.
 
 class Book:
     def __init__(self, title, author, price):
@@ -10,8 +16,12 @@ class Book:
         self.price = price
 
     # TODO: use the __str__ method to return a string
+    def __str__(self):
+        return f"{self.title} by {self.author}, costs {self.price}"
 
     # TODO: use the __repr__ method to return an obj representation
+    def __repr__(self):
+        return f"title={self.title},author{self.author},price={self.price}"
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
@@ -19,3 +29,5 @@ b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
 print(b1)
 print(b2)
+print(str(b1))
+print(repr(b2))
